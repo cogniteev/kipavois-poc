6da437fe98f93f9d054dee9ae47d792b10c561d7
Tristan Carel <tristan@cogniteev.com>
2015-08-19 17:15:08 +0200

    Flask proxy uses a chunked iterator

--- flask/app.py
+++ flask/app.py
@@ -19,6 +19,29 @@ ALL_METHODS = [
     'OPTIONS',
 ]
 MONITOR_SESSION = requests.Session()
+CHUNK_SIZE = 16 * 1024
+
+def chunked_response_iterator(resp, native_chunk_support, line_based):
+    """
+    Return stream with chunked encoding if native_chunk_support is True.
+    """
+    if line_based:
+        for chunk in resp.iter_lines(1):
+            chunk += '\n'
+            if native_chunk_support:
+                yield chunk
+            else:
+                yield hex(len(chunk))[2:] + '\r\n' + chunk + '\r\n'
+        if not native_chunk_support:
+            yield '0\r\n\r\n'
+    else:
+        for chunk in resp.iter_content(CHUNK_SIZE):
+            if native_chunk_support:
+                yield chunk
+            else:
+                yield hex(len(chunk))[2:] + '\r\n' + chunk + '\r\n'
+        if not native_chunk_support:
+            yield '0\r\n\r\n'
 
 
 @app.route('/monitor', defaults={'url': ''}, methods=ALL_METHODS)
@@ -26,6 +49,10 @@ MONITOR_SESSION = requests.Session()
 def monitor_proxy(url):
     params = dict(request.args.items())
     headers = dict(request.headers.items())
+    headers.update({
+        'x-kibana-user': 'google',
+        'Referer': 'http://localhost:5000/monitor/' + url
+    })
     addr = 'http://kibana:5601/' + url
     req = MONITOR_SESSION.request(
         request.method,
@@ -35,10 +62,24 @@ def monitor_proxy(url):
         params=params,
         data=request.data
     )
-    return Response(
-        stream_with_context(req.iter_content()),
-        content_type=req.headers['content-type']
-    )
+    # content_type = req.headers.get('Content-type', '')
+    # line_based = content_type.startswith(('text/', 'application/json'))
+    # gunicorn/werkzeug supports chunked encoding, no need to
+    # encode it manually
+    # native_chunk_support = (
+    #     'gunicorn' in request.environ['SERVER_SOFTWARE'] or
+    #     'Werkzeug' in request.environ['SERVER_SOFTWARE']
+    # )
+    headers = dict(req.headers)
+    if headers.get('transfer-encoding', None) == 'chunked':
+        stream_reader = chunked_response_iterator(req, True, False)
+        # do not pass the gzip header if provided by kibana
+        # because content is uncompressed when reading the stream.
+        headers.pop('content-encoding', None)
+    else:
+        stream_reader = stream_with_context(req.iter_content())
+    return Response(stream_reader, headers=headers,
+                    direct_passthrough=True, status=req.status_code)
 
 
 @app.route('/monitor/', methods=ALL_METHODS)
@@ -47,4 +88,4 @@ def monitor_proxy_home():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
